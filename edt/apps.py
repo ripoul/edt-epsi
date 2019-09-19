3d7e43648b5187012832b761ed67c0d435712710
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-19 20:03:29 +0200

    lint

--- edt/apps.py
+++ edt/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class EdtConfig(AppConfig):
-    name = 'edt'
+    name = "edt"
--- edt/views.py
+++ edt/views.py
@@ -14,9 +14,11 @@ def index(request):
         "action": "posEDTBEECOME",
         "serverid": "C",
         "Tel": idUser,
-        "date": today.strftime("%m/%d/%Y")
+        "date": today.strftime("%m/%d/%Y"),
     }
 
-    r = requests.request("GET", "https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx", params=payload)
+    r = requests.request(
+        "GET", "https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx", params=payload
+    )
 
     return redirect(r.url)
